e5796c83485dd6459f6a441e06c56fc2255ef36c
Lukas Juskevicius <ljuskevicius2002@gmail.com>
2024-02-13 21:52:20 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit TEST <code>src/App.js</code> and save to reload.
+          Edit TEST TEST <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
