faa3e5a5249da61dfc5cc061b652bc8de27bcb7c
Lukas Juskevicius <ljuskevicius2002@gmail.com>
2024-03-15 23:17:37 +0100

    barbers visual prototype

--- frontend/src/components/barbers/index.js
+++ frontend/src/components/barbers/index.js
@@ -1,22 +1,67 @@
+import * as React from 'react';
 import "./index.css";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import CircularProgress from '@mui/material/CircularProgress';
 import axios from 'axios';
+import { useState, useEffect } from 'react'; 
 
 function Barbers() {
-    const getBarbersWithServices = () => {
-        axios.get("http://localhost:5037/Barbers/Services")
-        .then(res => {
-            console.log(res)
-        }).catch(err => {
-            console.log(err)
-        })
-    }
-    
-    return (
+  const [barbers, setBarbers] = useState([]); 
+  const [isLoading, setIsLoading] = useState(false); 
 
-        <div>
-            <button className="butan" onClick={getBarbersWithServices}>Get Barbers With Service to Console</button>    
+  const getBarbersWithServices = async () => {
+    try {
+      setIsLoading(true);
+      const res = await axios.get("http://localhost:5037/Barbers/Services");
+      setBarbers(res.data);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getBarbersWithServices(); 
+  }, []); 
+
+  return (
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}> 
+      {isLoading ? 
+        <div style={{ width: '100%', textAlign: 'center', marginTop: 20 }}> 
+          <CircularProgress />
         </div>
-    );
-  }
-  
-  export default Barbers;
+      :  
+        barbers.map((barber) => (
+          <Card sx={{ maxWidth: 345, margin: 2, flex: '1 0 30%' }} key={barber.barberId}> 
+            <CardActionArea>
+                <div className="image-placeholder">
+                    <CardMedia
+                        component="img"
+                        height="140"
+                        image={barber.photoUrl || 'https://upload.wikimedia.org/wikipedia/commons/a/af/Default_avatar_profile.jpg'} 
+                        alt={barber.name}
+                        className="my-card-image"
+                    />
+                </div>
+                    <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {barber.name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {barber.bio} 
+                    </Typography>
+                    </CardContent>
+                </CardActionArea>
+          </Card>
+        ))
+      } 
+    </div>
+  );
+}
+
+export default Barbers;
