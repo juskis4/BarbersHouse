d7bde6893de23e0aa2c307948c97d5fc56c09800
Lukas Juskevicius <ljuskevicius2002@gmail.com>
2024-05-15 23:58:22 +0200

    cleanup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -48,7 +48,7 @@ const App = () => {
     {
       path:"/admin",
       element:(
-        <Suspense fallback={<div>Loading...</div>}> {/* Wrap in Suspense */}
+        <Suspense fallback={<div>Loading...</div>}> 
           <ProtectedRoute>
             <AdminPage /> 
           </ProtectedRoute>
