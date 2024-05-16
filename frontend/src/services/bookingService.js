ddb2aab07e7f1c9627798d66e40277a0226d27ae
Lukas Juskevicius <ljuskevicius2002@gmail.com>
2024-05-16 12:33:08 +0200

    admin dashboard page

--- frontend/src/services/bookingService.js
+++ frontend/src/services/bookingService.js
@@ -4,7 +4,7 @@ import utc from "dayjs/plugin/utc";
 
 dayjs.extend(utc);
 
-const apiUrl = process.env.REACT_APP_API_KEY;
+const apiUrl = "https://api-zdmjnhdz7q-ew.a.run.app";
 
 export async function getBarbersWithServices() {
   try {
