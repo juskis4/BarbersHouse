308946ef4af4b13ef06d7a87b41aeb5eaf2c3d43
Lukas Juskevicius <ljuskevicius2002@gmail.com>
2024-05-16 12:33:49 +0200

    misrtake fix

--- frontend/src/services/bookingService.js
+++ frontend/src/services/bookingService.js
@@ -4,7 +4,7 @@ import utc from "dayjs/plugin/utc";
 
 dayjs.extend(utc);
 
-const apiUrl = "https://api-zdmjnhdz7q-ew.a.run.app";
+const apiUrl = process.env.REACT_APP_API_KEY;
 
 export async function getBarbersWithServices() {
   try {
