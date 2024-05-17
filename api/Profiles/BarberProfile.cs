b479909b43bbc1bd25c8a2db87be777bee877b0c
Lukas Juskevicius <ljuskevicius2002@gmail.com>
2024-05-17 15:01:17 +0200

    added email to barbers

--- api/Profiles/BarberProfile.cs
+++ api/Profiles/BarberProfile.cs
@@ -15,6 +15,7 @@ public class BarberProfile : Profile
         CreateMap<Barber, BarberResultViewModel>()
         .ForMember(dest => dest.BarberId, opt => opt.MapFrom(src => src.BarberID))
         .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+        .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
         .ForMember(dest => dest.Bio, opt => opt.MapFrom(src => src.Bio))
         .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.PhotoUrl));
     }
--- api/ViewModels/BarberResultViewModel.cs
+++ api/ViewModels/BarberResultViewModel.cs
@@ -4,6 +4,7 @@ public class BarberResultViewModel
 {
     public int BarberId { get; set; }
     public string Name { get; set; }
+    public string Email {get; set;}
     public string Bio { get; set; }
     public string PhotoUrl { get; set; }
 }
