e3b85917e8e4f2b3da14bea8da0fb37b746d8f9d
felixioan <74063400+felixioan@users.noreply.github.com>
2024-05-18 14:36:44 +0200

    Main header

--- frontend/src/components/public/index.js
+++ frontend/src/components/public/index.js
@@ -3,10 +3,12 @@ import NavBar from './navbar';
 import Barbers from './barbers';
 import CarouselBarber from './carousel'; 
 import Box from '@mui/material/Box';
+import Header from './header';
 
 function PublicPage() {
     return (
         <Box sx={{ width: "100vw", display: "flex", flexDirection: "column" }}>
+            <Header/>
             <NavBar /> 
             <CarouselBarber /> 
             <Barbers /> 
