2d941c2cbbb95d09321f5c72015a40ac36e695dc
Lukas Juskevicius <ljuskevicius2002@gmail.com>
2024-05-22 23:35:22 +0200

    Service model update

---
+++ api/Migrations/20240522212808_nullablePriceNdescription.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class nullablePriceNdescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "price",
+                schema: "public",
+                table: "services",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "description",
+                schema: "public",
+                table: "services",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "price",
+                schema: "public",
+                table: "services",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "description",
+                schema: "public",
+                table: "services",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
